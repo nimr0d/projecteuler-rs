bfa0f002e4e481a7e722b79386bd867ef447c1d6
Nimrod Hajaj <hrodenticus@gmail.com>
2016-02-26 03:05:17 -0500

    Small change

--- src/euler/misc.rs
+++ src/euler/misc.rs
@@ -1,5 +1,5 @@
 /// Integer square root.
-pub fn int_sqrt(mut n : u64) -> u32 {
+pub fn int_sqrt(mut n : u64) -> u64 {
 	let mut res : u64 = 0;
 	let mut bit : u64 = 1 << 62;
 
@@ -17,10 +17,10 @@ pub fn int_sqrt(mut n : u64) -> u32 {
 		}
 		bit >>= 2;
 	}
-	return res as u32;
+	return res;
 }
 
 /// Triangle number
-pub fn tri(n : u64) -> u64{
+pub fn tri(n : u64) -> u64 {
 	return ((n + 1) * n) / 2;
 }
