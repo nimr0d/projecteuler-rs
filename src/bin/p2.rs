b18a95d7c5e940100b131f4e2ec3add47fd44598
Nimrod Hajaj <hrodenticus@gmail.com>
2016-02-26 02:55:30 -0500

    Added p2

--- src/bin/p2.rs
+++ src/bin/p2.rs
@@ -2,5 +2,12 @@
 extern crate euler;
 
 fn main() {
-	
+	let N = 4000000;
+	let mut f = (0, 1);
+	let mut sum = 0;
+	while f.0 <= N {
+		sum += f.0;
+		f = (f.0 + 2 * f.1, 2 * f.0 + 3 * f.1);
+	}
+	println!("{}", sum);
 }
